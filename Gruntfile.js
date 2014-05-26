53522f30fd112d8bb6394f8e702d1b401646bc76
Matthew Dahl <dahl.matthew@gmail.com>
2014-05-26 02:16:00 -0700

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    copy: {
+      all: {
+        files: [{
+          'css/bootstrap.min.css': 'bower_components/bootstrap/dist/css/bootstrap.min.css',
+          'js/bootstrap.min.js': 'bower_components/bootstrap/dist/js/bootstrap.min.js',
+          'js/jquery.min.js': 'bower_components/jquery/dist/jquery.min.js'
+        }, {
+          expand: true,
+          flatten: true,
+          src: ['bower_components/bootstrap/dist/fonts/**'],
+          dest: 'fonts/',
+          filter: 'isFile'
+        }]
+      }
+    }
+  });
+
+  require('load-grunt-tasks')(grunt);
+
+  // Default task(s).
+  grunt.registerTask('default', ['copy']);
+};
